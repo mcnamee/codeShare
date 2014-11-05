226d2a5bc4582d60d1d808bf1a861d335129bc66
Matt Mcnamee <matt@mcnamee.co>
2014-11-06 07:54:59 +1000

    added example file

---
+++ snippets/example.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ *	Name: Your Title
+ *	Author: Your Name
+ *  Description:
+ 		 Your Desc...
+ */
+
+$html = 
+'
+';
+
+$css =
+'
+';
+
+$js = 
+'
+';
+
+$plugins = array(
+    'js' => array(
+        'http://www.mcnam.ee/example.js',
+        'http://www.mcnam.ee/example2.js'
+    ),
+    'css' => array(
+        'http://www.mcnam.ee/example.css',
+        'http://www.mcnam.ee/example2.css'
+    )
+);
